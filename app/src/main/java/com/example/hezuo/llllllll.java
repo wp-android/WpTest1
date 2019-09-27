3dbcf1f2bac4faa3ee2d5b434241d1f4899b84a9
dem <480521572@qq.com>
2019-09-27 14:22:57 +0800

    Signed-off-by: dem <480521572@qq.com>

---
+++ app/src/main/java/com/example/hezuo/llllllll.java
@@ -0,0 +1,13 @@
+package com.example.hezuo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class llllllll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llllllll);
+    }
+}
