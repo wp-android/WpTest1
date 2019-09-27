7734863cb96571badd2868169525439d2a90b57a
weipeng <1593763981@qq.com>
2019-09-27 14:27:35 +0800

    Signed-off-by: weipeng <1593763981@qq.com>

--- app/src/main/java/com/example/hezuo/wp.java
+++ app/src/main/java/com/example/hezuo/wp.java
@@ -3,11 +3,11 @@ package com.example.hezuo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class lcb extends AppCompatActivity {
+public class wp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lcb);
+        setContentView(R.layout.activity_wp);
     }
 }
